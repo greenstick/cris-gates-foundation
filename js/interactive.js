a28354ea4a9514d5685ed66c405bd8a9dce44b26
Ben Cordier <bcordier@columnfivemedia.com>
2014-01-30 18:39:22 -0800

    V 0.0.3
    Base JS functional
    SVG’s structured and in - made need to adopt alternative method (bitmapping) depending on level of browser support required
    Leaflets styled structurally - portion needs to be done for Select Indicators section, browser support specs

--- js/interactive.js
+++ js/interactive.js
@@ -6,10 +6,11 @@ $(document).ready(function () {
 
 	var Slideshow = function () {
 	 	var slideshow = this;
-		 	slideshow.index = null;
+		 	slideshow.index = null,
+		 	slideshow.activeSlide = null;
 
 		/**
-		 *	Slideshow Methods
+		 *	Slideshow Init
 		 **/
 
 		 	slideshow.init = function () {
@@ -23,39 +24,82 @@ $(document).ready(function () {
 					slideshow.prev();
 				})
 				$('.exitmodal').on("click", function () {
-					slideshow.exitmodal();
+					slideshow.dismissModal();
+				})
+				$('.icon.home').on("click", function () {
+					slideshow.home();
+				})
+				$('.icon.cycle').on("click", function () {
+					slideshow.cycle();
 				})
 			};
 		 	//Next Slide
 		 	slideshow.next = function () {
-		 		console.log("nom");
 				$('.active').removeClass('active').next('.slide').addClass('active');
+				$('#interactiveWrapper').scrollTo($('.slide.active'), 600);
 		 	};
 		 	//Previous Slide
 		 	slideshow.prev = function () {
 				$('.active').removeClass('active').prev('.slide').addClass('active');
+				$('#interactiveWrapper').scrollTo($('.slide.active'), 600);
 		 	};
 		 	//Exit Modal
-		 	slideshow.dismissmodal = function () {
+		 	slideshow.dismissModal = function () {
 				$(this).parent('.modal').fadeOut(600);
+		 	};
+		 	//To Home
+		 	slideshow.home = function () {
+		 		$('.active').removeClass('active');
+		 		$('.slide:eq(0)').addClass('active');
+		 		$('#interactiveWrapper').scrollTo($('.slide.active'), 600);
+		 	};
+		 	//To Cycle
+		 	slideshow.cycle = function () {
+		 		$('.active').removeClass('active');
+		 		$('.slide:eq(2)').addClass('active');
+		 		$('#interactiveWrapper').scrollTo($('.slide.active'), 600);
 		 	};
 
 	};
+
+	/**
+	 *	Leaflet Class
+	 **/
+
 	var Leaflet = function () {
 		var leaflet = this;
 
 		/**
-		 *	Leaflet Methods
+		 *	Leaflet Init
 		 **/
 
 			leaflet.init = function () {
-
+				console.log("Leaf");
 			};
-	}
+
+		/**
+		 *	Leaflet Methods
+		 **/
+		 	//Slide Previous Leaflet Left
+		 	leaflet.left = function () {
+
+		 	};
+		 	//Slide Next Leaflet Right
+		 	leaflet.right = function () {
+
+		 	};
+		 	//Show Active Stem
+		 	leaflet.stemActive = function (element) {
+
+		 	};
+	};
 
 	/**
 	 *	Intialize Slideshow
 	 **/
-	 var interactive = new Slideshow();
+
+	var interactive = new Slideshow();
+	var leaflet = new Leaflet();
 		interactive.init();
-	});
+		leaflet.init();
+});
