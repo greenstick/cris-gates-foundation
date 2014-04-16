9366fc41088f93d5c919505f97ff98c0293e7f86
Ben Cordier <bcordier@columnfivemedia.com>
2014-04-15 18:23:12 -0700

    Version 0.0.8

--- js/interactive.js
+++ js/interactive.js
@@ -1,7 +1,27 @@
 $(document).ready(function () {
 	
+/*
+Global Functions
+*/
+
+//General Animation Method
+var animElem = function (options) {
+	var selector = options.selector,
+		duration = options.duration,
+	animation = {};
+		animation[options.attr] = options.value;
+		return $(selector).stop().animate(animation, duration);
+};
+//General Styling Method
+var styleElem = function (options) {
+	var selector = options.selector,
+	style = {};
+		style[options.attr] = options.value;
+		return $(selector).css(style);
+};
+
 /**
- *	Slideshow Class
+ *	Slideshow
  **/
 
 var Slideshow = function (leaflet) {
@@ -10,6 +30,7 @@ var Slideshow = function (leaflet) {
 	 	slideshow.engage = ko.observable(false),
 	 	slideshow.first = false,
 	 	slideshow.ring = false,
+	 	slideshow.contextsActive = false,
 	 	slideshow.leaflet = leaflet;
 	 	leaflet.slideshow = slideshow;
 
@@ -46,28 +67,35 @@ var Slideshow = function (leaflet) {
 	 	slideshow.next = function () {
 			$('.active').removeClass('active').next('.slide').addClass('active');
 			$('#interactiveWrapper').scrollTo($('.slide.active'), 600);
+			var position = $('.slide.active').position();
+			styleElem({selector: ".mask", attr: "left", value: position.left});
 			slideshow.index++;
 	 	};
 	 	//Previous Slide
 	 	slideshow.prev = function () {
 			$('.active').removeClass('active').prev('.slide').addClass('active');
 			$('#interactiveWrapper').scrollTo($('.slide.active'), 600);
+			var position = $('.slide.active').position();
+			styleElem({selector: ".mask", attr: "left", value: position.left});
 			slideshow.index--;
 	 	};
-	 	slideshow.showModal = function () {
-	 		$('.active .mask').stop().fadeIn();
-	 		$('.active .modal').stop().fadeIn();
+	 	// Show Modal
+	 	slideshow.showModal = function (modal) {
+	 		$('.mask').stop().fadeIn();
+	 		$(modal).stop().fadeIn();
 	 	};
 	 	//Exit Modal
-	 	slideshow.dismissModal = function () {
-	 		$('.active .mask').fadeOut(600);
-	 		$('.active .modal').fadeOut(600);
+	 	slideshow.dismissModal = function (modal) {
+	 		$('.mask').fadeOut(600);
+	 		$(modal).fadeOut(600);
 	 	};
 	 	//To Home
 	 	slideshow.home = function () {
 	 		$('.active').removeClass('active');
 	 		$('.slide:eq(0)').addClass('active');
 	 		$('#interactiveWrapper').scrollTo($('.slide.active'), 600);
+	 		var position = $('.slide.active').position();
+			styleElem({selector: ".mask", attr: "left", value: position.left});
 	 		slideshow.index = 0;
 	 	};
 	 	//To Cycle
@@ -75,11 +103,13 @@ var Slideshow = function (leaflet) {
 	 		$('.active').removeClass('active');
 	 		$('.slide:eq(2)').addClass('active');
 	 		$('#interactiveWrapper').scrollTo($('.slide.active'), 600);
+	 		var position = $('.slide.active').position();
+			styleElem({selector: ".mask", attr: "left", value: position.left});
 	 		slideshow.index = 2;
 	 	};
 	 	//Manages Hiding and Showing of Leaflet
 	 	slideshow.displayLeaflet = function () {
-	 		slideshow.index > 0 && slideshow.index != 3 ? slideshow.leaflet.show() : slideshow.leaflet.hide();
+	 		slideshow.index > 0 ? slideshow.leaflet.show() : slideshow.leaflet.hide();
 	 	};
 	 	//Shows Mask Overlay
 	 	slideshow.showMask = function (selector) {
@@ -105,7 +135,7 @@ var Slideshow = function (leaflet) {
 	 		$('.info').fadeOut();
 	 		$('.context').fadeOut();
 	 		$('.context-bg').fadeIn();
-	 		d3.select('#RING').attr('class', 'selected');
+	 		d3.select('#RING').attr('class', 'hover');
 	 		$('.' + id + '.context').fadeIn();
 	 	};
 	 	//Exit Context
@@ -117,6 +147,14 @@ var Slideshow = function (leaflet) {
 	 		$('.context-bg').fadeOut();
 	 		$('.context').fadeOut();
 	 		$('.info').fadeOut();
+	 	};
+	 	slideshow.toContext = function (element) {
+	 		var target = element.dataset.to;
+	 		$('.context').fadeOut();
+	 		$('.' + target).fadeIn();
+	 		d3.select('.eicHead').attr('class', 'eicHead');
+	 		d3.select('#RING').attr('class', 'hover');
+	 		d3.select('#' + target).attr('class', 'hover eicHead');
 	 	};
 	};
 
@@ -155,38 +193,26 @@ var Leaflet = function () {
 	 	};
 	 	//Slide Out Leaflet Contents
 	 	leaflet.slideOut = function () {
-	 		$('.nav').stop().fadeOut(600);
-	 		leaflet.styleElem({selector: '#leafletWrapper', attr: "width", value: "900px", duration: 600});
-	 		leaflet.styleElem({selector: '.tspStem', attr: "background-color", value: "#600922", duration: 600});
-	 		leaflet.animElem({selector: ".sliding", attr: "left", value: "50px", duration: 600});
+	 		animElem({selector: '#leafletWrapper', attr: "width", value: "900px", duration: 800});
+	 		styleElem({selector: '.tspStem', attr: "background-color", value: "#600922"});
+	 		animElem({selector: ".sliding", attr: "left", value: "50px", duration: 600});
+	 		//Show Mask & Set Nav z-index
+	 		styleElem({selector: ".nav", attr: "z-index", value: 9999});
+	 		$('.mask').fadeIn();
 	 		leaflet.active = true;
 	 	};
 	 	//Slide In Leaflet Contents
 	 	leaflet.slideIn = function () {
-	 		$('.nav').stop().fadeIn(600);
-	 		leaflet.styleElem({selector: '#leafletWrapper', attr: "width", value: "50px", duration: 600});
-	 		leaflet.styleElem({selector: '.tspStem', attr: "background-color", value: "#9a3b47", duration: 600});
-	 		leaflet.animElem({selector: ".sliding", attr: "left", value: "-550px", duration: 600});
+	 		animElem({selector: '#leafletWrapper', attr: "width", value: "50px", duration: 800});
+	 		styleElem({selector: '.tspStem', attr: "background-color", value: "#9a3b47"});
+	 		animElem({selector: ".sliding", attr: "left", value: "-550px", duration: 600});
 	 		leaflet.active = false;
-	 	};
-	 	//General Animation Method
-	 	leaflet.animElem = function (options) {
-	 		var selector = options.selector,
-	 			duration = options.duration,
-				animation = {};
-	 			animation[options.attr] = options.value;
-	 			return $(selector).stop().animate(animation, duration);
-	 	};
-	 	//General Styling Method
-	 	leaflet.styleElem = function (options) {
-	 		var selector = options.selector,
-	 			duration = options.duration,
-				animation = {};
-	 			animation[options.attr] = options.value;
-	 			return $(selector).css(animation, duration);
 	 	};
 	 	//Show Active Stem
 	 	leaflet.stemActive = function (element) {
+
+	 	};
+	 	leaflet.resetPosition = function () {
 
 	 	};
 	};
@@ -200,10 +226,9 @@ var Leaflet = function () {
 		interactive.init();
 
 	/**
-	 *	Slideshow Class Event Bindings
+	 *	Slideshow Event Bindings
 	 **/
 
-	 	//Binding Event Handlers
 		$('.next').on("click", function () {
 			interactive.next();
 			interactive.displayLeaflet();
@@ -229,45 +254,64 @@ var Leaflet = function () {
 			interactive.showInfo(e.currentTarget.id);
 			d3.select(this).attr('class', 'eicArc selected');
 		});
-		$('.eicHead').on("click", function (e) {
-			interactive.showContext(e.currentTarget.id);
-			d3.select(this).attr('class', 'eicHead selected');
-			d3.select('#RING').attr('class', 'selected');
-			interactive.ring = true;
-		});
 		$('.context .infoExit').on("click", function () {
 			interactive.exitContext();
+			interactive.contextsActive = false;
 		});
 		$('.eicHead').on("mouseover", function () {
-			d3.select('#RING').attr('class', 'selected');
+			d3.select('#RING').attr('class', 'hover');
 		});
 		$('.eicHead').on("mouseout", function () {
 			interactive.ring === false ? d3.select('#RING').attr('class', '') : void(0)
 		});
+		$('#CONTEXTS_button').on("mouseover", function () {
+			if (interactive.contextsActive === false) {
+				d3.select('#RING').attr("class", "hover");
+				d3.select('#CONTEXTS').attr("class", "hover eicHead");
+			}
+		});
+		$('#CONTEXTS_button').on("mouseout", function () {
+			if (interactive.contextsActive === false) {
+				d3.select('#RING').attr("class", "");
+				d3.select('#CONTEXTS').attr("class", "eicHead");
+			}
+		});
+		$('#CONTEXTS_button').on("click", function () {
+			interactive.contextsActive = true;
+			interactive.showContext("CONTEXTS");
+		});
+		$('.n, .p').on("click", function (e) {
+			interactive.toContext(e.currentTarget);
+		});
 
 	/**
-	 *	Leaflet Class Event Bindings
+	 *	Leaflet Event Bindings
 	 **/
 
 		$('.tspStem').on("click", function () {
 			leaflet.toggle();
 		});
-		$('.tspNext, .siStem').on("click", function () {
-			leaflet.animElem({selector: ".tspLeaf", attr: "left", value: "-500px", duration: 600});
-			leaflet.animElem({selector: ".siStem", attr: "left", value: "-275px", duration: 600});
-			leaflet.styleElem({selector: '.siStem', attr: "background-color", value: "#600922", duration: 600});
-
+		$('.tspNext').on("click", function () {
+			animElem({selector: ".tspLeaf", attr: "left", value: "-500px", duration: 600});
+			animElem({selector: ".siStem", attr: "left", value: "-275px", duration: 600});
+			styleElem({selector: '.siStem', attr: "background-color", value: "#600922"});
+		});
+		$('.siStem').on("click", function () {
+			animElem({selector: ".tspLeaf", attr: "left", value: "-500px", duration: 600});
+			animElem({selector: ".siStem", attr: "left", value: "-275px", duration: 600});
+			styleElem({selector: '.siStem', attr: "background-color", value: "#600922"});
 		});
 		$('.siNext .button').on("click", function () {
-			leaflet.animElem({selector: ".eciStem", attr: "left", value: "275px", duration: 600});
-			leaflet.animElem({selector: ".siLeaf .slider", attr: "left", value: "-500px", duration: 600});
+			animElem({selector: ".eciStem", attr: "left", value: "275px", duration: 600});
+			animElem({selector: ".siLeaf .slider", attr: "left", value: "-500px", duration: 600});
 		});
 		$('.eciStem').on("click", function () {
-			$('.slide.active')
 			leaflet.slideshow.next();
-			leaflet.styleElem({selector: '#leafletWrapper', attr: "width", value: "100px", duration: 600});
-			leaflet.animElem({selector: ".eciStem", attr: "left", value: "-275px", duration: 600});
-			leaflet.animElem({selector: ".siLeaf", attr: "left", value: "-450px", duration: 600});
-			leaflet.styleElem({selector: '.eciStem', attr: "background-color", value: "#600922", duration: 600});
+			animElem({selector: '#leafletWrapper', attr: "width", value: "100px", duration: 800});
+			animElem({selector: ".siLeaf", attr: "left", value: "-452px", duration: 600});
+			animElem({selector: ".eciStem", attr: "left", value: "-275px", duration: 600});
+			styleElem({selector: '.eciStem', attr: "background-color", value: "#600922"});
+			styleElem({selector: ".nav", attr: "z-index", value: 10});
+			$('.mask').fadeOut();
 		});
 });
